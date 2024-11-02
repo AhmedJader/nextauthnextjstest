ec7e8d2ed3f797e7d1e36827391e526f161f572c
Ahmed <Ahmedjader837@gmail.com>
2024-11-02 01:55:07 -0400

    created register form field.

--- src/app/register/Form.tsx
+++ src/app/register/Form.tsx
@@ -1,10 +1,113 @@
 "use client"
 
 import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: "This field has to be filled."
+  })
+  .email("Please enter a valid email address.")
+  .max(300, {
+    message: "Password must be less than 300 characters."
+  }),
+  Password: z.string().min(6, {
+    message: "Password must be at least 6 characters."
+  }),
+  confirmPassword: z.string().min(6, {
+    message: "Password must be at least 6 characters."
+  })
+}).superRefine(({Password, confirmPassword}, ctx) => {
+  if (Password !== confirmPassword) {
+    ctx.addIssue({
+      code: "custom",
+      message: "Passwords do not match.",
+      path: ["confirmPassword"]
+    })
+  }
+});
 
 const RegisterForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {},
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+
   return (
-    <div>form</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name ="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="lorumipsum@email.com" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your email.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name ="Password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your password, DO NOT SHARE IT. 
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name ="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormDescription>
+                Re-type your password in to confirm. 
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
